343d9021933f25d2ecc534b1684d47e7f45e7b28
tesh-bhatia <nbhatia03@gmail.com>
2017-12-07 13:45:56 -0500

    Adds click event to characters, random HP/AP/CP given on creation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 function Character(charname){
     this.name = charname;
-    this.HP = 100;
-    this.AP = 4;
-    this.CP = 7;
+    this.HP = Math.floor(Math.random() * 50) + 101;
+    this.AP = Math.floor(Math.random() * 4) + 4;
+    this.CP = Math.floor(Math.random() * 4) + 5;
     this.img = "assets/images/" + charname + ".png";
 }
 
-
-var charNames = ['Luke', 'Vader', 'Boba'];
+//*IMPORTANT* These names need to match up EXACTLY with img names to work *IMPORTANT*
+var charNames = ['Luke', 'Vader', 'Boba', 'Han', 'Ewok'];
 
 var characters = charNames.map(function(v,i){
     var char  = new Character(v);
@@ -24,8 +24,25 @@ var makeCharacter = function(character){
     .addClass('character');
     img.attr("src", character.img)
     .addClass('charImg');
-    characterDiv.prepend("<h3 class='charName'>" + character.name + "</h3>");
+    characterDiv.prepend("<h3 class='charName'>" + character.name + "</h3>")
+    .append("<h4 class='charHP'>" + character.HP + "</h4>");
+    characterDiv.attr({
+        "data-HP": character.HP,
+        "data-AP": character.AP,
+        "data-CP": character.CP
+    })
     $('#characters').append(characterDiv);
+    characterDiv.one('click', function(){
+        var character = $(this);
+        $('#player').prepend(character);
+        var charNum = $('#characters > .character').length;
+        var randCharNum = Math.floor( (Math.random() * charNum) + 1 )
+        $('#opponent').append($('#characters > .character:nth-child(' + randCharNum + ')'));
+        $('.character').off();
+        character.attr("id","playerChar")
+        .find('img').addClass("flipped");
+        $('#attack').removeClass("invisible");
+    })
 
 }
 
